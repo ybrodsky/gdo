ce3f019053f983626dbe8f50b0afca916b36fced
Yael J. Brodsky <brodskyy88@gmail.com>
2017-11-28 16:51:01 -0300

    borradas

--- frontend/app/components/user/UserResource.js
+++ frontend/app/components/user/UserResource.js
@@ -4,23 +4,7 @@ gdoApp.factory('User', function($resource, globalVariables) {
   }, {
     update: {
       method: 'PUT'
-    },
-    cuenta: {
-      method: 'PUT',
-      url: globalVariables.url + '/user/cuenta/:id',
-    },
-    unseen: {
-      url: globalVariables.url + '/user/unseen',
-      method: 'GET',
-    },
-    autocomplete: {
-      method: 'GET',
-      url: globalVariables.url + '/user',
-      isArray: true,
-      headers: {
-        ajaxLoader: false
-      }
-    },
+    }
   });
 });
 
